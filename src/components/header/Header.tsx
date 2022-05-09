8fa1c591888e53b3f0e0027acaf7444f3eed0e45
KangMin Her <81793748+nicecarrot@users.noreply.github.com>
2022-05-10 00:27:32 +0900

    basic structure

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/header/_header.module.scss";
+
+const Header = () => {
+
+    return(
+        <header className={styles.header}> ??? </header>
+
+    );
+}
+
+export default Header;
---
+++ src/components/main/Section.tsx
@@ -0,0 +1,9 @@
+const Section = () => {
+    return(
+        <main>
+            
+        </main>
+    );
+};
+
+export default Section;
