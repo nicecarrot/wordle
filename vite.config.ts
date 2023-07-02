42d0036587a257897fb12ccd67090116a78cf769
nicecarrot <nicecarrot20@gmail.com>
2023-07-02 14:25:50 +0900

    add docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/wordle/',
   plugins: [react()],
   build: {
+    outDir: 'docs', // 배포할 때 생성되는 폴더명을 'docs'로 변경
     rollupOptions: {
-      input: 'src/Main.tsx'
-    }
-  }
-})
+      input: '/src/Main.tsx',
+    },
+  },
+});
