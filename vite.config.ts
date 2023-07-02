5b5ec263d9c10c589c376d1b94d4b861de804b80
nicecarrot <nicecarrot20@gmail.com>
2023-07-02 19:41:14 +0900

    plugin update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    process.env.MODE !== 'production' ? react({
+      jsxRuntime: 'classic',
+    }) : react(),
+    svgLoader(),
+  ],
   mode: "development",
   build: {
     minify: false,
