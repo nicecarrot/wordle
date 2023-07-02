9632852038ff917cd585fb82b3cf12f8f58a507b
nicecarrot <nicecarrot20@gmail.com>
2023-07-02 14:00:41 +0900

    Modify 4

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/wordle/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['src/Main.tsx']
+    }
+  }  
 })
