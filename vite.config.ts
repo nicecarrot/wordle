0a109523895959f1ecf7c5083715532c5f8a55d6
nicecarrot <nicecarrot20@gmail.com>
2023-07-02 20:03:26 +0900

    버전 업그레이드

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,62 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgLoader from 'vite-svg-loader';
+import { createRequire } from 'module';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    process.env.MODE !== 'production' ? react({
-      jsxRuntime: 'classic',
-    }) : react(),
-    svgLoader(),
-  ],
-  mode: "development",
-  build: {
-    minify: false,
-  }
+const require = createRequire(import.meta.url);
+
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  const baseConfig = {
+    base: '/',
+    plugins: [
+      react({
+        jsxRuntime: 'classic',
+      }),
+      svgLoader(),
+    ],
+    mode: 'development',
+    build: {
+      minify: false,
+    },
+  };
+
+  const customConfig = {
+    build: {
+      rollupOptions: {
+        output: {
+          // Vite 기본값인 dynamicImportFunction을 사용하려면 주석 처리하세요.
+          // dynamicImportFunction: false,
+        },
+        plugins: [
+          {
+            name: 'commonjs-transform',
+            transform: (code, id) => {
+              if (id.includes('react-phone-input-2')) {
+                const isModuleExports = (id) => {
+                  try {
+                    const module = require(id);
+                    if (module?.default) {
+                      return false;
+                    }
+                    return 'auto';
+                  } catch (error) {
+                    return 'auto';
+                  }
+                };
+                return {
+                  code: `module.exports = ${isModuleExports.toString()};`,
+                  map: null,
+                };
+              }
+              return code;
+            },
+          },
+        ],
+      },
+    },
+  };
+
+  return isProduction ? { ...baseConfig, ...customConfig } : baseConfig;
 });
